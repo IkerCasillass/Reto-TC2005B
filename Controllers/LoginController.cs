cc2261d079a1635f423312a0a226b51da3859a74
NelOsdJuaDua <A01025317@tec.mx>
2023-10-16 19:35:55 -0600

    Acceso a login
    El login en parte html es accesible, solo falta implementar el jason para hacerlo funcional

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebLogin.Models;
+
+namespace WebReto.Controllers
+{
+    public class LoginController:Controller
+    {
+        public IActionResult BaseLogin()
+        {
+            return View();
+        }
+    }
+
+    
+}
