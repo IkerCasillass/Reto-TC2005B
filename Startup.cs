e6a57c1478024ad33d67e5fd6d922625e4d4f534
NelOsdJuaDua <A01025317@tec.mx>
2023-10-18 16:39:55 -0600

    Archivos del juego
    Comparto los archivos del juego, en las carpetas Build, TemplateData y el archivo index.html

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -52,6 +53,21 @@ namespace WebReto
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseHttpsRedirection();
+
+            app.UseDefaultFiles();
+            app.UseFileServer();
+            StaticFileOptions options = new StaticFileOptions();
+            FileExtensionContentTypeProvider contentTypeProvider = (FileExtensionContentTypeProvider)options.ContentTypeProvider ?? new FileExtensionContentTypeProvider();
+            contentTypeProvider.Mappings.Add(".unityweb", "application/octet—stream");
+            options.ContentTypeProvider = contentTypeProvider; 
+            app.UseStaticFiles(options);
+
+            app.UseMvc();
+
         }
+
+
     }
 }
